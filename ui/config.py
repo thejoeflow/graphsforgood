c9d4e871ba2ebdb83800c8fae6454bf6d9c39897
shreya.rajput <shreya.rajput@filament.uk.com>
2019-12-08 20:58:23 -0500

    should work

--- ui/config.py
+++ ui/config.py
@@ -8,5 +8,6 @@ lambda_function_names = {
     'register_new_graph': 'register_new_graph',
     'edit_graph': 'edit_graph',
     'schedule_email': 'schedule_email',
-    'get_registered_graph': 'get_registered_graph'
+    'get_registered_graph': 'get_registered_graph',
+    'update_data' : 'update_data'
 }
